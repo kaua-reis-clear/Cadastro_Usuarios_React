315a4dbdbe7f4dc9d4aec192b0ea0172e7fd3336
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-27 10:50:26 -0300

    created Home component.

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -6,14 +6,17 @@ import React from 'react'
 
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
-import Main from '../components/template/Main'
+import Home from '../components/home/Home'
 import Footer from '../components/template/Footer'
 
 export default props =>
     <div className="app">
         <Logo />
         <Nav />
-        <Main icon="home" title="Início"
-            subtitle="Segundo Projeto do capítulo de React."/>
+        <Home icon="home" title="Início"
+            subtitle="Segundo Projeto do capítulo de React.">
+            <div className="display-4">Bem Vindo!</div>
+            <p className="mb-0">Sistema para exemplificar a construção de um cadastro desenvolvido em React</p>
+        </Home>
         <Footer />
     </div>
