ab168e548fe9756db805fd339fd47294a1ad1216
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-27 11:08:22 -0300

    implemented routes

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Main from '../template/Main'
 
 export default props =>
-<Main icon="home" title="Início"
+    <Main icon="home" title="Início"
     subtitle="Segundo Projeto do capítulo de React.">
         <hr />
         <p className="mb-0">Sistema para exemplificar a construção de um cadastro desenvolvido em React!</p>
--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,22 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
-
 import './App.css'
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
-import Home from '../components/home/Home'
+import Routes from './routes'
 import Footer from '../components/template/Footer'
 
 export default props =>
-    <div className="app">
-        <Logo />
-        <Nav />
-        <Home icon="home" title="Início"
-            subtitle="Segundo Projeto do capítulo de React.">
-            <div className="display-4">Bem Vindo!</div>
-            <p className="mb-0">Sistema para exemplificar a construção de um cadastro desenvolvido em React</p>
-        </Home>
-        <Footer />
-    </div>
+    <BrowserRouter>
+        <div className="app">
+            <Logo />
+            <Nav />
+            <Routes />
+            <Footer />
+        </div>
+    </BrowserRouter>
+    
